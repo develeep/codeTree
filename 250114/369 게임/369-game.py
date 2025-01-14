92e8dbf29b8c45620fd25e7e72ba391fc63e58d0
Develeep <83402978+develeep@users.noreply.github.com>
2025-01-14 23:36:11 +0900

    Title: 369 게임, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/369 게임/369-game.py
+++ 250114/369 게임/369-game.py
@@ -1,7 +1,18 @@
 n = int(input())
 
+def find3(i):
+    if i % 10 == 0:
+        return False
+    if (i % 10 )%3 == 0:
+        return True
+    
+    return find3(i//10)
+
 for i in range(1,n+1):
-    if (i % 10 != 0 and (i%10)%3 == 0) or i%3 == 0:
-        print(0, end=' ')
+    if i % 3 == 0:
+        print(0,end=' ')
+        continue
+    if find3(i):
+        print(0,end=' ')
         continue
     print(i, end = ' ')
