e3cdcd44248f4065e3f70e64549acabfb19662d8
Develeep <83402978+develeep@users.noreply.github.com>
2025-01-30 23:43:48 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250130/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = list(map(int,input().split()))
+
+cnt = 0
+
+for idx,num in enumerate(arr):
+    if num == 2:
+        cnt += 1
+    if cnt==3:
+        print(idx+1)
+        break
