4d13811fe3aeef6666d5ecaddc94972805e6112c
Develeep <83402978+develeep@users.noreply.github.com>
2025-01-31 16:54:20 +0900

    Title: 배열로 사각형 만들기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250131/배열로 사각형 만들기/print-array-in-rectangle.py
+++ 250131/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -1,22 +1,8 @@
-arr = [[0 for _ in range(5)]for _ in range(5)]
+arr = [[1 for _ in range(5)]for _ in range(5)]
 
-for i in range(5):
-    cnt = 1
-    plus = i
-    plus2 = 6
-    for j in range(5):
-        arr[i][j] = cnt
-        if i == 1:
-            cnt += 1
-        elif i == 2:
-            cnt += i + j
-        elif i == 3:
-            cnt += plus
-            plus += i + j
-        elif i == 4:
-            cnt += plus
-            plus += plus2 
-            plus2 += i+j
-            
+for i in range(1,5):
+    for j in range(1,5):
+        arr[i][j] = arr[i-1][j] + arr[i][j-1]
+    
 for i in arr:
     print(*i)
