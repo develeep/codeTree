4aa16765bd86e47fbeb7fdb859cebd2de19af3fd
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-03 14:24:30 +0900

    Title: 문자열 길이의 합, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250203/문자열 길이의 합/sum-length-of-string.py
+++ 250203/문자열 길이의 합/sum-length-of-string.py
@@ -2,5 +2,5 @@ n = int(input())
 
 arr = [input() for _ in range(n)]
 hap = sum(len(i) for i in arr)
-a = len([i for i in arr if i[0] == 'a'])
+a = sum(1 for i in arr if i.startswith('a'))
 print(hap, a)
