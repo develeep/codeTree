deb03d3f70f59838d2e93236cab0e8d2c01b0c97
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-20 20:50:31 +0900

    Title: 흥미로운 숫자 2, Time: 175ms, Memory: 18MB, Status: Passed - CodeTree

--- 250220/흥미로운 숫자 2/interesting-numbers-2.py
+++ 250220/흥미로운 숫자 2/interesting-numbers-2.py
@@ -3,16 +3,16 @@ X, Y = map(int, input().split())
 # Write your code here!
 cnt = 0
 for i in range(X,Y+1):
-    arr = [n for n in str(i)]
-    s = set(arr)
-    if len(s) == 2:
-        dir = {}
-        for S in s:
-            dir[S] = 0
-        for a in arr:
-            dir[a] += 1
-        for i in dir:
-            if dir[i] == 1:
-                cnt += 1
+    arr = []
+    while i > 0:
+        arr.append(i % 10)
+        i //= 10
+    s = list(set(arr))
+    hap = sum(arr)
+    len_arr = len(arr)
+    if len(s) != 2:
+        continue
+    if (s[0]*(len_arr-1) + s[1]) == hap or (s[1]*(len_arr-1) + s[0]) == hap:
+        cnt += 1 
 
 print(cnt)
