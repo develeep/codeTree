e7cef71f6fac3b9e282e77f979c6502366bf3b67
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-20 11:24:15 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250220/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 250220/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -2,18 +2,18 @@ A = input()
 cnt = 0
 
 # Write your code here!
-def check(new_arr):
+def check(new_arr,index):
     global cnt
     if len(new_arr) == 4:
         cnt += 1
         return
-    for i in range(len(A)):
+    for i in range(index,len(A)):
         if len(new_arr) == 2:
             if i + 1 < len(A) and A[i] == A[i+1] == ')':
-                check(new_arr+[')',')'])
+                check(new_arr+[')',')'],i+1)
         else:
-            if i + 1 < len(A) and A[i] == A[i+1] == '(':
-                check(new_arr+['(','('])
+            if i + 1 < len(A)-2 and A[i] == A[i+1] == '(':
+                check(new_arr+['(','('],i+1)
 
-check([])
+check([],0)
 print(cnt)
