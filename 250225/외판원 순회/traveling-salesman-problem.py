1bb50ddb272a3b1fee6f4e9fb9e919b095295ed2
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-25 23:59:24 +0900

    Title: 외판원 순회, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250225/외판원 순회/traveling-salesman-problem.py
+++ 250225/외판원 순회/traveling-salesman-problem.py
@@ -2,26 +2,29 @@ n = int(input())
 A = [list(map(int, input().split())) for _ in range(n)]
 
 # Write your code here!
-def find_min(idx,cnt,cost):
-    if len(cost) and cost[-1] == 0:
-        return
+def find_min(cnt,cost):
     if cnt == n:
         global min_result
-        if A[idx][0] == 0:
+        total = 0
+        for i in range(n-1,0,-1):
+            a = A[cost[i]][cost[i-1]]
+            if a == 0:
+                return
+            total += a
+        last = A[cost[-1]][0]
+        if last == 0:
             return
-        cost.append(A[idx][0])
-        min_result = min(min_result,sum(cost))
+        total += last
+        min_result = min(min_result,total)
     
     for i in range(1,n):
-        if A[idx][i] == 0:
-            continue
         if visited[i] == 0:
             visited[i] = 1
-            find_min(i,cnt+1,cost+[A[idx][i]])
+            find_min(cnt+1,cost+[i])
             visited[i] = 0
 
 min_result = float('inf')
 visited = [0]*n
-find_min(0,1,[])
+find_min(1,[0])
 
 print(min_result)
