05a513a4ceb96f6da20184fcb18549071742c12f
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-25 23:28:29 +0900

    Title: 외판원 순회, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250225/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,21 @@
+n = int(input())
+A = [list(map(int, input().split())) for _ in range(n)]
+
+# Write your code here!
+def find_min(idx,cnt,cost):
+    if cnt == n:
+        global min_result
+        cost += A[idx][0]
+        min_result = min(min_result,cost)
+    
+    for i in range(1,n):
+        if visited[i] == 0:
+            visited[i] = 1
+            find_min(i,cnt+1,cost+A[idx][i])
+            visited[i] = 0
+
+min_result = float('inf')
+visited = [0]*n
+find_min(0,1,0)
+
+print(min_result)
