27931ba08ccbf6a9983333050c0d7a24a8506798
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-25 22:34:52 +0900

    Title: 1차원 윷놀이, Time: 5086ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 250225/1차원 윷놀이/yutnori-1d.py
+++ 250225/1차원 윷놀이/yutnori-1d.py
@@ -4,27 +4,25 @@ nums = list(map(int, input().split()))
 # Write your code here!
 visited = [0]*n
 max_result = 0
-def find_max(arr):
+def find_max(arr,m_cnt,move_cnt):
     global max_result
     if max_result >= k:
         return
     if len(arr) == n:
-        cnt = 0
-        move_cnt = 1
-        for move in arr:
-            move_cnt += move
-            if move_cnt >= m:
-                cnt += 1
-                move_cnt = 1
-        max_result = max(max_result,cnt)
+        if move_cnt >= m:
+            m_cnt += 1
+        max_result = max(max_result,m_cnt)
         return
     for i in range(n):
         if max_result >= k:
-            break
+            return
         if visited[i] == 0:
             visited[i] = 1
-            find_max(arr + [nums[i]])
+            if move_cnt >= m:
+                find_max(arr + [nums[i]], m_cnt+1, 1 + nums[i])
+            else:
+                find_max(arr + [nums[i]], m_cnt, move_cnt + nums[i])
             visited[i] = 0
 
-find_max([])
+find_max([],0,1)
 print(max_result)
