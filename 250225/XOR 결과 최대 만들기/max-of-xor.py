522c57d2735fb76e25d8afbdbc61020b0dbe66ed
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-25 23:04:48 +0900

    Title: XOR 결과 최대 만들기, Time: 74ms, Memory: 19MB, Status: Passed - CodeTree

--- 250225/XOR 결과 최대 만들기/max-of-xor.py
+++ 250225/XOR 결과 최대 만들기/max-of-xor.py
@@ -3,19 +3,16 @@ A = list(map(int, input().split()))
 
 # Write your code here!
 
-def find_max(idx,arr):
-    if len(arr) == m:
+def find_max(idx,cnt,val):
+    if cnt == m:
         global max_result
-        result = arr[0]
-        for i in range(1,len(arr)):
-            result = result ^ arr[i]
-        max_result = max(max_result, result)
+        max_result = max(max_result, val)
         return
 
     for i in range(idx,n):
-        find_max(i+ 1, arr+[A[i]])
+        find_max(i+ 1,cnt+1, val ^ A[i])
 
 max_result = float('-inf')
-find_max(0,[])
+find_max(0,0,0)
 
 print(max_result)
