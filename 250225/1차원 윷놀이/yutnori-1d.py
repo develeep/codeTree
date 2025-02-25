de6227d1e28a487d1e1b3315fe02953f471a5ee4
Develeep <83402978+develeep@users.noreply.github.com>
2025-02-25 17:05:22 +0900

    Title: 1차원 윷놀이, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250225/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,26 @@
+n, m, k = map(int, input().split())
+nums = list(map(int, input().split()))
+
+# Write your code here!
+positions = [1]*k
+
+max_result = 0
+def find_max(idx):
+    if idx == n:
+        global max_result
+        cnt = 0
+        for pos in positions:
+            if pos >= m:
+                cnt += 1
+        max_result = max(max_result,cnt)
+        return
+    move = nums[idx]
+    for i in range(k):
+        if positions[i] < m:
+            positions[i] += move
+            find_max(idx+1)
+            positions[i] -= move
+    
+        
+find_max(0)
+print(max_result)
