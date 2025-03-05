388adbafa5d02c1d3ab34dba1057e85d89435784
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-05 15:49:43 +0900

    Title: 자리 차지하기, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250305/자리 차지하기/take-place.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+used = [0]*m
+# Please write your code here.
+a.sort()
+
+for i in range(m-1,-1,-1):
+    if len(a) == 0:
+        break
+    while 1:
+        user = a.pop()
+        if i >= user:
+            used[i] = 1
+            break
+
+print(sum(used))
