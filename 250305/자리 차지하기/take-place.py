b0b9ff165d904a79045de46bc9b10f3c12b79743
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-05 16:08:58 +0900

    Title: 자리 차지하기, Time: 5030ms, Memory: 27MB, Status: Time Limit Exceed - CodeTree

--- 250305/자리 차지하기/take-place.py
+++ 250305/자리 차지하기/take-place.py
@@ -2,17 +2,16 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 used = [0]*m
 # Please write your code here.
-a.sort(reverse=True)
+def sit(user):
+    for u in range(user-1,-1,-1):
+        if used[u] == 0:
+            used[u] = 1
+            return 1
+    return 0
 
-for i in range(m):
-    if len(a) == 0:
+for ar in a:
+    if sit(ar) == 0:
         break
-    while 1:
-        if len(a) == 0:
-            break
-        user = a.pop()
-        if i < user:
-            used[i] = 1
-            break
+
 
 print(sum(used))
