1781dd624eb0c4f5288d869594e9fd89aef39c97
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-05 15:54:45 +0900

    Title: 자리 차지하기, Time: 82ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 250305/자리 차지하기/take-place.py
+++ 250305/자리 차지하기/take-place.py
@@ -2,16 +2,16 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 used = [0]*m
 # Please write your code here.
-a.sort()
+a.sort(reverse=True)
 
-for i in range(m-1,-1,-1):
+for i in range(m):
     if len(a) == 0:
         break
     while 1:
         if len(a) == 0:
             break
         user = a.pop()
-        if i >= user:
+        if i+1 <= user:
             used[i] = 1
             break
 
