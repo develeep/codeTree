67c65f63b87fc7ead0772e7bdfb048da7d5ed0c2
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-12 10:27:32 +0900

    Title: 최단 거리 3, Time: 100ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 250312/최단 거리 3/shortest-distance-3.py
+++ 250312/최단 거리 3/shortest-distance-3.py
@@ -1,13 +1,3 @@
-n, m = map(int, input().split())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
-A, B = map(int, input().split())
-
-# Please write your code here.
-nodes [set() for _ in range(n+1)]
-for start, end, length in edges:
-    nodes[start].add((end, length))
-    nodes[end].add((start, length))
-
 import heapq
 def bfs(start):
     q = [(0, start)]
@@ -16,9 +6,26 @@ def bfs(start):
     min_depth[start] = 0
     while q:
         depth, node = heapq.heappop(q)
+        if node == B:
+            return depth
         for neighbor, length in nodes[node]:
             m = depth + length
             if min_depth[neighbor] > m:
                 min_depth[neighbor] = m
-                heapq.heappush(q, (neighbor, m))
-            
+                heapq.heappush(q, (m, neighbor))
+    return False
+
+
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+A, B = map(int, input().split())
+
+# Please write your code here.
+nodes = [set() for _ in range(n+1)]
+for start, end, length in edges:
+    nodes[start].add((end, length))
+    nodes[end].add((start, length))
+
+
+
+print(bfs(A))        
