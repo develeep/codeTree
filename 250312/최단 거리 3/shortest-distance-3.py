4114e3c52025643d2a31b1e8e0fe06d5822b948d
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-12 10:25:01 +0900

    Title: 최단 거리 3, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250312/최단 거리 3/shortest-distance-3.py
+++ 250312/최단 거리 3/shortest-distance-3.py
@@ -3,5 +3,22 @@ edges = [tuple(map(int, input().split())) for _ in range(m)]
 A, B = map(int, input().split())
 
 # Please write your code here.
+nodes [set() for _ in range(n+1)]
+for start, end, length in edges:
+    nodes[start].add((end, length))
+    nodes[end].add((start, length))
+
+import heapq
 def bfs(start):
-    
+    q = [(0, start)]
+    visited = [0]*(n+1)
+    min_depth = [100000]*(n+1)
+    min_depth[start] = 0
+    while q:
+        depth, node = heapq.heappop(q)
+        for neighbor, length in nodes[node]:
+            m = depth + length
+            if min_depth[neighbor] > m:
+                min_depth[neighbor] = m
+                heapq.heappush(q, (neighbor, m))
+            
