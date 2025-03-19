e337faf1b47ceb3ccd901dadb96e18e185cbdab7
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-19 17:04:36 +0900

    Title: 그래프 탐색, Time: 99ms, Memory: 20MB, Status: Passed - CodeTree

--- 250319/그래프 탐색/graph-traversal.py
+++ 250319/그래프 탐색/graph-traversal.py
@@ -2,8 +2,17 @@ n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 nodes = [[] for _ in range(n+1)]
 # Please write your code here.
+def dfs(node):
+    visited[node] = 1
+    c = 1
+    for neighbor in nodes[node]:
+        if visited[neighbor] == 1:
+            continue
+        c += dfs(neighbor)
+    return c
+
 for start, end in edges:
     nodes[start].append(end)
     nodes[end].append(start)
-
-dfs(1)
+visited = [0]*(n+1)
+print(dfs(1)-1)
