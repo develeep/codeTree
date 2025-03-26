f6f2bd08874d5a3c0cb27060c9f8177a048f9c38
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-26 22:51:33 +0900

    Title: 갈 수 있는 곳들, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250326/갈 수 있는 곳들/places-can-go.py
+++ 250326/갈 수 있는 곳들/places-can-go.py
@@ -5,7 +5,13 @@ points = [tuple(map(int, input().split())) for _ in range(k)]
 # Please write your code here.
 from collections import deque
 
+dxy = ((0,1),(0,-1),(-1,0),(1,0))
 def bfs():
     q = deque()
+    visited = [[0]*n for _ in range(n)]
     for r,c in points:
-        q.append((r,c,0))
+        q.append((r-1,c-1))
+        visited[r-1][c-1] = 1
+    while q:
+        r,c 
+    
