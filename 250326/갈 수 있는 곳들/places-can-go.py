f3b93412d4989138b342797dad3e4792c825f26f
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-26 22:53:55 +0900

    Title: 갈 수 있는 곳들, Time: 64ms, Memory: 17MB, Status: Passed - Codetree

--- 250326/갈 수 있는 곳들/places-can-go.py
+++ 250326/갈 수 있는 곳들/places-can-go.py
@@ -9,9 +9,23 @@ dxy = ((0,1),(0,-1),(-1,0),(1,0))
 def bfs():
     q = deque()
     visited = [[0]*n for _ in range(n)]
+    cnt = 0
     for r,c in points:
         q.append((r-1,c-1))
         visited[r-1][c-1] = 1
     while q:
-        r,c 
+        r,c = q.popleft()
+        cnt += 1
+        for dx,dy in dxy:
+            nx,ny = r + dx, c + dy
+            if not(0<=nx<n and 0<=ny<n):
+                continue
+            if grid[nx][ny] or visited[nx][ny]:
+                continue
+            
+            visited[nx][ny] = 1
+            q.append((nx,ny))
+    print(cnt)
+
+bfs()
     
