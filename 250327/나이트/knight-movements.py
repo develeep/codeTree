b19cf0aff13f1a18cb50b1d5acd1f62206cb8b6a
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-27 20:28:47 +0900

    Title: 나이트, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250327/나이트/knight-movements.py
@@ -0,0 +1,27 @@
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+
+# Please write your code here.
+from collections import deque
+dxy = ((-1,-2),(-1,2),(1,2),(1,-2),(-2,-1),(-2,1),(2,1),(2,-1))
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y,0))
+    visited = [[0]*n for _ in range(n)]
+    visited[x][y] = 1
+    while q:
+        x,y,depth = q.popleft()
+
+        for dx,dy in dxy:
+            nx,ny = x+dx,y+dy
+            if not(0<=nx<n and 0<=ny<n):
+                continue
+            if (nx,ny) == (r2-1,c2-1):
+                return depth + 1
+            if visited[nx][ny]:
+                continue
+            visited[nx][ny] = 1
+            q.append((nx,ny,depth + 1))
+    return -1
+print(bfs(r1-1,c1-1))
