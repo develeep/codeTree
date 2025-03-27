d6bd00b990dea79a25f59eecf1a7e27baf69c2d6
Develeep <83402978+develeep@users.noreply.github.com>
2025-03-27 19:18:06 +0900

    Title: K번 최댓값으로 이동하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250327/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -0,0 +1,8 @@
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+r, c = map(int, input().split())
+
+# Please write your code here.
+
+def bfs(x,y):
+    q = 
